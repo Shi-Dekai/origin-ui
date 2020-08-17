72d68bc8b0fa94309cc9cf2bd112b1d11765c7ce
shi <1727259503@qq.com>
2020-08-17 22:11:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Button/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Button from '@/components/Button.vue';
+
+@Component({
+  components: {
+    Button
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>

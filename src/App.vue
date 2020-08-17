ab5fd609d3e42b1e7135a48645af31448851ff9f
shi <1727259503@qq.com>
2020-08-17 22:22:52 +0800

    完成按钮基本样式

--- src/App.vue
+++ src/App.vue
@@ -17,5 +17,12 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+  #app{
+    margin: 20px;
+  }
 
+  *{box-sizing: border-box; margin: 0; padding:0;}
+  body{
+    font-size: 14px;
+  }
 </style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    hi
+    <button class="g-button">按钮</button>
   </div>
 </template>
 
@@ -15,5 +15,21 @@
 </script>
 
 <style lang="scss" scoped>
-  
+  .g-button{
+    font-size: 14px;
+    height: 32px;
+    padding: 0 1em;
+    border-radius: 4px;
+    border: 1px solid #999;
+    background: white;
+    &:hover{
+      border-color: #666;
+    }
+    &:active{
+      background-color: #eee;
+    }
+    &:focus{
+      outline: none;
+    }
+  }
 </style>

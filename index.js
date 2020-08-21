b14bc7ad6afd5101714aceb414091fe5750e8f3c
shi <1727259503@qq.com>
2020-08-21 11:50:16 +0800

    更新package.json

---
+++ index.js
@@ -0,0 +1,5 @@
+import Button from '@/components/Button.vue'
+import ButtonGroup from '@/components/Button-group.vue'
+import Icon from '@/components/Icon.vue'
+
+export default {Button, ButtonGroup, Icon}

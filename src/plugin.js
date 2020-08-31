213ccddf4b25cf3f83151c40ad66fc15b6283cb5
shi <1727259503@qq.com>
2020-08-31 22:32:15 +0800

    实现了只能有一个 toast 的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,27 @@
 import Toast from '@/components/Toast.vue'
 
+let currentToast
+
+function createToast({Vue, propsData, message}) {
+  const Constructor = Vue.extend(Toast)
+  const toast = new Constructor({propsData})
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
+}
+
 export default {
   install(Vue, options) {
     Vue.prototype.$toast = function (message, toastOptions) {
-      const Constructor = Vue.extend(Toast)
-      const toast = new Constructor({
-        propsData: toastOptions
-      })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue, propsData: toastOptions, message})
     }
   }
 }
+
+
+
+
